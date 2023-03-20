2f656e0802983dc356db0e0ccf31649206ecfcf3
PC7-IFLAB <53550730+rifqi10@users.noreply.github.com>
2023-03-20 07:50:48 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ modul5_1302210094/Penjumlahan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302210094
+{
+    internal class Penjumlahan
+    {
+   
+
+        public void JumlahTigaAngka<T>(T data1, T data2, T data3){
+            T jml;
+            jml = (dynamic)data1 + (dynamic)data2 + (dynamic)data3;
+            Console.WriteLine(" Hasil Penjumlahan " + jml);
+        }
+
+    }
+}
